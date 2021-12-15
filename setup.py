b9dab88ed56d5ac9b885236cb998b78b01d5bff2
niranda perera <niranda.perera@gmail.com>
2021-12-15 16:37:48 -0500

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cylon-flow',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='niranda perera',
+    author_email='niranda@niranda.dev',
+    description=''
+)
